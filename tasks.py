32aa239e7c356ac20894f7db672b8150cb022cae
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2025-04-28 08:26:18 -0300

    feat: adding arguments to set_chrome_options

--- tasks.py
+++ tasks.py
@@ -26,7 +26,7 @@ class OEMCatalogScraper:
 
     def set_chrome_options(self):
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-extensions')
         options.add_argument('--disable-gpu')
@@ -34,6 +34,7 @@ class OEMCatalogScraper:
         options.add_argument('--start-maximized')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--log-level=3")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return options
 
