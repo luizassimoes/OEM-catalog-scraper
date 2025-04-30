40e277935fe3f1dee714e2496e288c0b7e6fdc7c
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2025-04-30 15:48:45 -0300

    docs: adding docstring for each function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import re
 import os
 import sys
 import time
@@ -9,8 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from concurrent.futures import ThreadPoolExecutor
+
 
 class OEMCatalogScraper:
 
@@ -24,8 +23,12 @@ class OEMCatalogScraper:
         self.download_dir = download_dir
         os.makedirs(os.path.dirname(self.download_dir), exist_ok=True)
 
-
     def set_chrome_options(self):
+        """
+        Configures Chrome options for Selenium WebDriver.
+        Returns:
+            options (ChromeOptions): The configured ChromeOptions object.
+        """
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
@@ -47,8 +50,12 @@ class OEMCatalogScraper:
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return options
 
-
     def set_webdriver(self):
+        """
+        Initializes the Chrome WebDriver with the specified options.
+        Returns:
+            None
+        """
         options = self.set_chrome_options()
         try:
             self.driver = webdriver.Chrome(options=options)
@@ -58,6 +65,13 @@ class OEMCatalogScraper:
             sys.exit()
 
     def open_url(self, url: str):
+        """
+        Opens the specified URL in the web browser using the initialized WebDriver.
+        Args:
+            url (str): The URL to be opened.
+        Returns:
+            None
+        """
         if self.driver:
             try:
                 self.driver.get(url)
@@ -67,6 +81,13 @@ class OEMCatalogScraper:
             self.logger.error('WebDriver not initialized. You must set_webdriver() first.')
 
     def scroll_down(self, selector):
+        """
+        Scrolls the page to bring the specified element into view.
+        Args:
+            selector (tuple): The locator for the element to scroll to (e.g., (By.XPATH, '...')).
+        Returns:
+            None
+        """
         try:
             element = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable(selector)
@@ -77,6 +98,16 @@ class OEMCatalogScraper:
             pass
 
     def get_products(self, url):
+        """
+        Fetches product details from a specified URL and extracts relevant information.
+        Info logs are generated for each successfully extracted product.
+        Errors are logged if the request fails or the URL is inaccessible.
+        Args:
+            url (str): The URL to fetch the product data from.
+        Returns:
+            list: A list of dictionaries, each containing product details including `product_id`, `name`, 
+                `description`, and `specs`.
+        """
         try:
             with requests.get(url, headers=self.headers) as response:
                 response.raise_for_status()
@@ -119,8 +150,15 @@ class OEMCatalogScraper:
             self.logger.error(f'Error trying to get products:\n{e}')
             sys.exit()
 
-    
     def get_bom(self, product_id):
+        """
+        Fetches the Bill of Materials (BOM) for a product from the specified URL and extracts part details.
+        Args:
+            product_id (str): The product ID used to form the URL to fetch the BOM.
+        Returns:
+            list: A list of dictionaries, each containing details about a part, including `part_number`, 
+                `description`, and `quantity`.
+        """
         self.open_url(f'https://www.baldor.com/catalog/{product_id}#tab="parts"')
         self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.data-table tbody tr')))
         bom_rows = self.driver.find_elements(By.CSS_SELECTOR, 'table.data-table tbody tr')
@@ -141,6 +179,14 @@ class OEMCatalogScraper:
         return bom_list
 
     def get_assets(self, product_id):
+        """
+        Fetches and downloads product assets including a manual (PDF), image, and CAD drawing (DWG) file.
+        Logs errors or warnings if any assets cannot be downloaded or if there are any issues during the process.
+        Args:
+            product_id (str): The product ID used to fetch the asset files.
+        Returns:
+            dict: A dictionary containing the file paths of the downloaded assets (manual, image, and CAD).
+        """
         assets = {"manual": '', "cad": '', "image": ''}
         assets_url = f'output/assets/{product_id}/'
         os.makedirs(os.path.dirname(assets_url), exist_ok=True)
@@ -228,6 +274,14 @@ class OEMCatalogScraper:
         return assets
 
     def processing_product(self, product):
+        """
+        Processes product data by fetching the BOM and assets, and saves the results to a JSON file.
+        Logs a warning if any assets or information are missing, otherwise logs a success message.
+        Args:
+            product (dict): A dictionary containing product information, including the 'product_id'.
+        Returns:
+            None: The function modifies the provided product dictionary and saves the results to a JSON file.
+        """
         product_id = product['product_id']
         self.logger.info(f'Processing product {product_id}.')
 
@@ -254,6 +308,14 @@ class OEMCatalogScraper:
             self.logger.info(log_msg)
 
     def close_all(self):
+        """
+        Closes the WebDriver session. 
+        Logs an error if the WebDriver fails to close or if it was not initialized.
+        Args:
+            None
+        Returns:
+            None
+        """
         if self.driver:
             try:
                 self.driver.quit()
@@ -262,19 +324,38 @@ class OEMCatalogScraper:
         else:
             self.logger.error('WebDriver not initialized.')
 
+
 def run_scraper_for_product(product):
+    """
+    Runs the scraper for a given product. 
+    Logs the processing of the product and any issues encountered during scraping.
+    Args:
+        product (dict): A dictionary containing product details, including `product_id`.
+    Returns:
+        None
+    """
     product_id = product['product_id']
     scraper = OEMCatalogScraper(download_dir=os.path.abspath(f'output/assets/{product_id}'))
     scraper.set_webdriver()
     scraper.processing_product(product)
     scraper.close_all()
 
+
 def main():
+    """
+    Main function to run the scraper for multiple products from a specific catalog.
+    Sets up the scraper, retrieves product data from the Baldor API, and processes each product concurrently 
+    using a ThreadPoolExecutor.
+    Logs the start and completion of product data retrieval, the number of products processed, 
+    and any issues or exceptions encountered during scraping.
+    Returns:
+        None
+    """
     scraper = OEMCatalogScraper()
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='output/0_OEM_Scraper.log')
     scraper.set_webdriver()
 
-    catalog_number = 110 # 16, 24, 69, 110, 312, 315...
+    catalog_number = 110  # Existing catalogs: 16, 24, 69, 110, 312, 315...
     num_products = 10
     url = f"https://www.baldor.com/api/products?include=results&language=en-US&pageIndex=3&pageSize={num_products}&category={catalog_number}"    
 
